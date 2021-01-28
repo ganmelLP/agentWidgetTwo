e7c43c79ced4021183edc7aef0a3068f287328dc
ganmelLP <ganm@liveperson.com>
2021-01-28 15:27:19 +0000

    fg

--- app.js
+++ app.js
@@ -72,8 +72,7 @@ document.onreadystatechange = () => {
                 fuelType.getElementsByTagName('option')[0].setAttribute("class", "fuel selection")
                } else {
 
-                document.getElementById("new").checked = false;
-                document.getElementById("used").checked = true;
+                document.getElementById("used").click;
 
                 optionBrand = SDEResult.vehicleOfInterest[0].voi.make;
                 usedBrandName.getElementsByTagName('option')[0].innerHTML = optionBrand;
